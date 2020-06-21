db6debb009ce82a5998685d68073092ce2c9995a
celioggr <celioggr@gmail.com>
2020-06-21 02:14:57 +0100

    oz tests simplified without unused contract functions

---
+++ test/oz_FuturXe.test.js
@@ -0,0 +1,103 @@
+const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
+const { ZERO_ADDRESS } = constants;
+
+const {
+  shouldBehaveLikeERC20,
+  shouldBehaveLikeERC20Transfer,
+  shouldBehaveLikeERC20Approve,
+} = require('./ERC20.behavior');
+
+const FuturXe = artifacts.require('FuturXe');
+
+contract('FuturXe', function ([_, initialHolder, recipient, anotherAccount]) {
+  const initialSupply = new BN(100);
+
+  beforeEach(async function () {
+    this.token = await FuturXe.new(1000,"FuturXe","FXE",10);
+  });
+
+  shouldBehaveLikeERC20('ERC20', initialSupply, initialHolder, recipient, anotherAccount);
+
+
+
+  describe('_mintToken', function () {
+    const amount = new BN(50);
+    it('rejects a null account', async function () {
+      await expectRevert(
+        this.token.mintToken(ZERO_ADDRESS, amount), 'ERC20: mint to the zero address'
+      );
+    });
+
+    describe('for a non zero account', function () {
+      beforeEach('minting', async function () {
+        const { logs } = await this.token.mintToken(recipient, amount);
+        this.logs = logs;
+      });
+
+      it('increments totalSupply', async function () {
+        const expectedSupply = initialSupply.add(amount);
+        expect(await this.token.totalSupply()).to.be.bignumber.equal(expectedSupply);
+      });
+
+      it('increments recipient balance', async function () {
+        expect(await this.token.balanceOf(recipient)).to.be.bignumber.equal(amount);
+      });
+
+      it('emits Transfer event', async function () {
+        const event = expectEvent.inLogs(this.logs, 'Transfer', {
+          from: ZERO_ADDRESS,
+          to: recipient,
+        });
+
+        expect(event.args.value).to.be.bignumber.equal(amount);
+      });
+    });
+  });
+
+  describe('_burn', function () {
+    it('rejects a null account', async function () {
+      await expectRevert(this.token.burn(ZERO_ADDRESS, new BN(1)),
+        'ERC20: burn from the zero address');
+    });
+
+    describe('for a non zero account', function () {
+      it('rejects burning more than balance', async function () {
+        await expectRevert(this.token.burn(
+          initialHolder, initialSupply.addn(1)), 'ERC20: burn amount exceeds balance'
+        );
+      });
+
+      const describeBurn = function (description, amount) {
+        describe(description, function () {
+          beforeEach('burning', async function () {
+            const { logs } = await this.token.burn(initialHolder, amount);
+            this.logs = logs;
+          });
+
+          it('decrements totalSupply', async function () {
+            const expectedSupply = initialSupply.sub(amount);
+            expect(await this.token.totalSupply()).to.be.bignumber.equal(expectedSupply);
+          });
+
+          it('decrements initialHolder balance', async function () {
+            const expectedBalance = initialSupply.sub(amount);
+            expect(await this.token.balanceOf(initialHolder)).to.be.bignumber.equal(expectedBalance);
+          });
+
+          it('emits Transfer event', async function () {
+            const event = expectEvent.inLogs(this.logs, 'Transfer', {
+              from: initialHolder,
+              to: ZERO_ADDRESS,
+            });
+
+            expect(event.args.value).to.be.bignumber.equal(amount);
+          });
+        });
+      };
+
+      describeBurn('for entire balance', initialSupply);
+      describeBurn('for less amount than balance', initialSupply.subn(1));
+    });
+  });
+});
