c96742b5926d872d00ee4faef22dd303f5ecb399
celioggr <celioggr@gmail.com>
2020-07-30 01:16:09 +0100

    Each contract has a truffle project on its own under test/

---
+++ test/BitAseanToken/test/oz_BitAseanToken.test.js
@@ -0,0 +1,59 @@
+const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
+const { ZERO_ADDRESS } = constants;
+
+const {
+  shouldBehaveLikeERC20,
+  shouldBehaveLikeERC20Transfer,
+  shouldBehaveLikeERC20Approve,
+} = require('./ERC20.behavior');
+
+
+
+const BitAseanToken = artifacts.require('BitAseanToken');
+
+contract('BitAseanToken', function ([_, initialHolder, recipient, anotherAccount]) {
+  const initialSupply = new BN(100);
+
+  beforeEach(async function () {
+    this.token = await BitAseanToken.new(100,"BitAseanToken",10,"BAT", {'from':initialHolder});
+  });
+
+  shouldBehaveLikeERC20('ERC20', initialSupply, initialHolder, recipient, anotherAccount);
+
+  /*
+  describe('_mint', function () {
+    const amount = new BN(50);
+    it('rejects a null account', async function () {
+      await expectRevert(
+        this.token.mintToken(ZERO_ADDRESS, amount), 'ERC20: mint to the zero address'
+      );
+    });
+
+    describe('for a non zero account', function () {
+      beforeEach('minting', async function () {
+        const { logs } = await this.token.mintToken(recipient, amount);
+        this.logs = logs;
+      });
+
+      it('increments totalSupply', async function () {
+        const expectedSupply = initialSupply.add(amount);
+        expect(await this.token.totalSupply()).to.be.bignumber.equal(expectedSupply);
+      });
+
+      it('increments recipient balance', async function () {
+        expect(await this.token.balanceOf(recipient)).to.be.bignumber.equal(amount);
+      });
+
+      it('emits Transfer event', async function () {
+        const event = expectEvent.inLogs(this.logs, 'Transfer', {
+          from: ZERO_ADDRESS,
+          to: recipient,
+        });
+
+        expect(event.args.value).to.be.bignumber.equal(amount);
+      });
+    });
+  });
+  */
+});
